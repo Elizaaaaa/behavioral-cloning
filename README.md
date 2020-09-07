b44a888c1b340b83fa1dec171ad258161106cfc6
Elizaaaaa <elizaaaaa@gmail.com>
2020-09-07 06:02:56 +0000

    retrain with my data

--- README.md
+++ README.md
@@ -32,8 +32,8 @@ You're not required to use markdown for your writeup.  If you use another method
 The Project
 ---
 The goals / steps of this project are the following:
-* Use the simulator to collect data of good driving behavior 
-* Design, train and validate a model that predicts a steering angle from image data
+* Use the simulator to collect dataof good driving behavior 
+* De sign, train and validate a model that predicts a steering angle from image data
 * Use the model to drive the vehicle autonomously around the first track in the simulator. The vehicle should remain on the road for an entire loop around the track.
 * Summarize the results with a written report
 
--- model.py
+++ model.py
@@ -1,15 +1,11 @@
 import numpy as np
-import cv2
-<<<<<<< HEAD
 from scipy import ndimage
-=======
->>>>>>> 4ae292760f0ed29fa0661cf43e84201fe703f409
 
 def load_images():
     import csv
     
     lines = []
-    with open('./data/driving_log.csv') as csvfile:
+    with open('/opt/data/driving_log.csv') as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
             lines.append(line)
@@ -17,15 +13,10 @@ def load_images():
     measurements = []
     for line in lines:
         filename = line[0].split('/')[-1]
-        path = './data/IMG/' + filename
-<<<<<<< HEAD
+        path = '/opt/data/IMG/' + filename
        # bgr_image = cv2.imread(path)
        # image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
         image = ndimage.imread(path)
-=======
-        bgr_image = cv2.imread(path)
-        image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
->>>>>>> 4ae292760f0ed29fa0661cf43e84201fe703f409
         images.append(image)
         measurements.append(float(line[3]))
       
@@ -37,7 +28,6 @@ def load_images():
     
 def train(x, y):
     from keras.models import Sequential
-<<<<<<< HEAD
     from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D
     
     model = Sequential()
@@ -55,18 +45,8 @@ def train(x, y):
     #model.add(Dense(1))
     
     model.compile(loss='mse', optimizer='adam')
-    model.fit(x, y, validation_split=0.2, nb_epoch=2, shuffle=True)
-=======
-    from keras.layers import Flatten, Dense
-    
-    model = Sequential()
-    model.add(Flatten(input_shape=(160,320,3)))
-    model.add(Dense(1))
-    
-    model.compile(loss='mse', optimizer='adam')
-    model.fit(x, y, validition_split=0.2, shuffle=True)
->>>>>>> 4ae292760f0ed29fa0661cf43e84201fe703f409
-    
+    model.fit(x, y, validation_split=0.2, epochs=2, shuffle=True)
+
     model.save('model.h5')
     
     
