8ab0e7531455fd1aa0ca466bccb90c210bdd32df
Elizaaaaa <elizaaaaa@gmail.com>
2020-09-07 06:43:34 +0000

    crop the image

--- model.py
+++ model.py
@@ -22,15 +22,15 @@ def load_images():
         images.append(np.fliplr(image))
         measurements.append(-steer)
         # Add other 2 camera images
-        filename = line[1].split('/')[-1]
-        limg = ndimage.imread(path+filename)
-        filename = line[2].split('/')[-1]
-        rimg = ndimage.imread(path+filename)
-        images.append(limg)
-        images.append(rimg)
-        correction = 0.1
-        measurements.append(steer+correction)
-        measurements.append(steer-correction)
+#         filename = line[1].split('/')[-1]
+#         limg = ndimage.imread(path+filename)
+#         filename = line[2].split('/')[-1]
+#         rimg = ndimage.imread(path+filename)
+#         images.append(limg)
+#         images.append(rimg)
+#         correction = 0.1
+#         measurements.append(steer+correction)
+#         measurements.append(steer-correction)
       
     X_train = np.array(images)
     y_train = np.array(measurements)
@@ -40,10 +40,11 @@ def load_images():
     
 def train(x, y):
     from keras.models import Sequential
-    from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D
+    from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Cropping2D
     
     model = Sequential()
-    model.add(Lambda(lambda x: (x/255.0) - 0.5, input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160,320,3)))
+    model.add(Lambda(lambda x: (x/255.0) - 0.5, input_shape=(90,320,3)))
     model.add(Conv2D(6, kernel_size=(5,5), activation='relu'))
     model.add(MaxPooling2D())
     model.add(Conv2D(6, kernel_size=(5,5), activation='relu'))
@@ -57,7 +58,7 @@ def train(x, y):
     #model.add(Dense(1))
     
     model.compile(loss='mse', optimizer='adam')
-    model.fit(x, y, validation_split=0.2, epochs=5, shuffle=True)
+    model.fit(x, y, validation_split=0.2, epochs=3, shuffle=True)
 
     model.save('model.h5')
     
