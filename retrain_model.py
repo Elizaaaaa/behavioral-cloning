fb7b520f59dd5a41e87939d916210d5ed9a99726
Elizaaaaa <elizaaaaa@github.com>
2020-09-08 04:55:00 +0000

    do more train on same model

---
+++ retrain_model.py
@@ -0,0 +1,75 @@
+import numpy as np
+from scipy import ndimage
+import csv
+
+def generator(samples, batch_size=32):
+    import sklearn
+    from random import shuffle
+    
+    n = len(samples)
+    
+    path = './data/IMG/'
+    correction = 0.2
+    while 1:
+        shuffle(samples)    
+        for i in range(0, n, batch_size):
+            batch_samples = samples[i:i+batch_size]
+            images = []
+            angles = []
+            for sample in batch_samples:
+                angle = float(sample[3])
+                filename = sample[0].split('/')[-1]
+                image = ndimage.imread(path+filename)
+                images.append(image)
+                angles.append(angle)
+                images.append(np.fliplr(image))
+                angles.append(-1. * angle)
+                
+                filename = sample[1].split('/')[-1]
+                image = ndimage.imread(path+filename)
+                images.append(image)
+                angles.append(angle+correction)
+              #  images.append(np.fliplr(image))
+              #  angles.append(-1 * (angle+correction))
+                
+                filename = sample[2].split('/')[-1]
+                image = ndimage.imread(path+filename)
+                images.append(image)
+                angles.append(angle-correction)
+              #  images.append(np.fliplr(image))
+              #  angles.append(-1 * (angle-correction))
+                
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+    
+def train():
+    from keras.models import Sequential
+    from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Cropping2D, Dropout
+    from sklearn.model_selection import train_test_split
+    from math import ceil
+    
+    samples = []
+    with open('./data/driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            samples.append(line)
+       
+    train_sample, validation_sample = train_test_split(samples, test_size=0.2)
+    
+    batch_size = 32
+    train_generator = generator(train_sample, batch_size=batch_size)
+    validation_generator = generator(validation_sample, batch_size=batch_size)
+    
+    model = Swquential()
+    model.load('model.h5')
+    
+    model.fit_generator(train_generator,
+                        steps_per_epoch=ceil(len(train_sample)/batch_size),
+                        validation_data=validation_generator,
+                        validation_steps=ceil(len(validation_sample)/batch_size),
+                        epochs=5, verbose=1)    
+    
+if __name__ == "__main__":
+    #X_train, y_train = load_images()
+    train()
